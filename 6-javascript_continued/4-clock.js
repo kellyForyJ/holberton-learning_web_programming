1835c1c257e743b176a87118ed405bbf81f5fda3
kellyForyJ <58444559+kellyForyJ@users.noreply.github.com>
2019-12-09 17:20:06 -0500

    reloj
    reloj

--- 6-javascript_continued/4-clock.js
+++ 6-javascript_continued/4-clock.js
@@ -1,20 +1,9 @@
 let hora = 0
-let hora2 = 1
 let minutos = 0;
-let minutos2 = 0;
-if (hora<=23) {
+
   console.log(hora)
-}
+
 while (minutos <= 59) {
   console.log(minutos)
   minutos++;
-
-}
-if (hora2<=23) {
-  console.log(hora2)
-}
-while (minutos2 <= 59) {
-  console.log(minutos2)
-  minutos2++;
-
 }
