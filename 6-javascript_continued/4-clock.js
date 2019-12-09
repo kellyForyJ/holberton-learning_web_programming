56e726c35ee8b090511e358a74c2b50133cb7bae
kellyForyJ <58444559+kellyForyJ@users.noreply.github.com>
2019-12-09 17:12:07 -0500

    reloj animado
    reloj para 0 y 1

--- 6-javascript_continued/4-clock.js
+++ 6-javascript_continued/4-clock.js
@@ -1,8 +1,16 @@
-let hora = process.argv[2];
+let hora = 0
+let hora2 = 1
 let minutos = 0;
+let minutos2 = 0;
   console.log(hora)
 while (minutos <= 59) {
   console.log(minutos)
   minutos++;
+
+}
+  console.log(hora2)
+while (minutos2 <= 59) {
+  console.log(minutos2)
+  minutos2++;
 
 }
