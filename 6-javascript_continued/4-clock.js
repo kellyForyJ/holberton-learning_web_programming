10f29c0531771463d1ed1b20806aa89d3e3e0a18
kellyForyJ <58444559+kellyForyJ@users.noreply.github.com>
2019-12-09 17:27:58 -0500

    reloj
    todas las horas

--- 6-javascript_continued/4-clock.js
+++ 6-javascript_continued/4-clock.js
@@ -1,9 +1,13 @@
 let hora = 0
 let minutos = 0;
+while (hora <= 23) {
 
   console.log(hora)
+  while (minutos <= 59) {
+    console.log(minutos)
+    minutos++;
 
-while (minutos <= 59) {
-  console.log(minutos)
-  minutos++;
+  }
+    hora++;
+    minutos = 0;
 }
